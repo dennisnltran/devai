4c133001f5ea91d14e35a71d6a0e2aa717dea472
Dennis Tran <dennis.tran@linksys.com>
2026-03-29 02:11:43 -0700

    Update simple workflow to run tests

---
+++ test/calculator.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+A simple calculator tool for testing.
+"""
+def add(a, b):
+    return a + b
---
+++ test/test_calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A test tool for calculator.
+"""
+
+from calculator import add
+
+def test_add_positive_numbers():
+    assert add(1, 2) == 3
+
+def test_add_negative_numbers():
+    assert add(-1, 1) == 0
